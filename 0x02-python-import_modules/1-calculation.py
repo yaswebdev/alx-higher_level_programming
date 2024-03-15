66e1062ebe514b8f4ff9c0ad7d947c90c8e5c401
yaswebdev <yassinerami034@gmail.com>
2024-03-14 17:57:53 -0700

    edit file 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add
-    from calculator_1 import sub
-    from calculator_1 import div
-    from calculator_1 import mul
-    a = 10
-    b = 5
+    from calculator_1 import add, sub, div, mul #import the functions (add,sub,div,mul) from a file calculator_1
+    a = 10 #define a variable 'a' and give him a value 10
+    b = 5 #define a variable 'b' and give him a value 5
     print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
     print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
     print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
